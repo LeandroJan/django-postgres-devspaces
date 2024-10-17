e27ba47f97b1ea53443f6144b9e50e7b3559531a
Leandro Jan <nt84h@gmail.com>
2024-10-17 09:24:42 -0300

    debugging

--- wait_db.py
+++ wait_db.py
@@ -5,7 +5,7 @@ from psycopg2 import OperationalError
 
 while True:
     try:
-        conn = psycopg2.connect(dbname='db', user='user', password='password', host='localhost', port='5432')
+        conn = psycopg2.connect(dbname='db', user='redhat', password='Redhat@123', host='localhost', port='5432')
         conn.close()
         print('PostgreSQL is ready!')
         break
